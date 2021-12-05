f86f49a6cc0bf7c092af4252100216647eaf1e96
Dan Tcheche <dtchechenistky@devartis.com>
2021-12-05 17:03:31 -0300

    Add decode url

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ VueJS Frontend for url shortener.
 - Run `npm install`
 - Run `npm run serve`
 - The project should run in port 8080 to avoid CORS related issues.
+
+
+### Notes
+
+If I had more time I would have created a backendService to make the posts and avoid code repetition.
+I also would've created more components and divide the app better.
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,17 +20,26 @@
                         <v-btn :loading="loading" :disabled="!isValid" @click="shortenUrl()">Shorten url</v-btn>
                     </v-col>
                 </v-row>
-                <h5 v-if="shortenedUrl" class="mt-3 mb-3">Your shortened url is: <span class="blue">{{ shortenedUrl }}</span></h5>
+                <h5 v-if="shortenedUrl" class="mt-3 mb-3">Your shortened url is: <a :href="shortenedUrl" target="_blank">{{ shortenedUrl }}</a></h5>
             </v-form>
             <h5>Or copy a shortened url to get the full one</h5>
-            <v-row>
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                    <url-field v-model="urlToDecode" />
-                </v-col>
-            </v-row>
+            <v-form v-model="isValidForm2">
+                <v-row>
+                    <v-col
+                        cols="5"
+                        md="4"
+                    >
+                        <url-field v-model="urlToDecode" />
+                    </v-col>
+                    <v-col
+                        cols="5"
+                        md="4"
+                    >
+                        <v-btn :loading="loading" :disabled="!isValidForm2" @click="decodeUrl()">Decode url</v-btn>
+                    </v-col>
+                    <h5 v-if="decodedUrl" class="mt-3 mb-3">Your decoded url is: <a :href="decodedUrl" target="_blank">{{ decodedUrl }}</a></h5>
+                </v-row>
+            </v-form>
         </v-container>
     </div>
 
@@ -50,7 +59,9 @@ export default {
             urlToDecode: null,
             loading: false,
             isValid: true,
-            shortenedUrl: null
+            isValidForm2: true,
+            shortenedUrl: null,
+            decodedUrl: null
         };
     },
     methods: {
@@ -61,9 +72,24 @@ export default {
                 if(!response.data.success) {
                     this.$notify({ type: "error", text: response.data.message });
                 } else {
-                    console.log(response.data.url.short_url);
                     this.urlToShorten = null;
                     this.shortenedUrl = `${settings.API_URL}${response.data.url.short_url}`
+                }
+            } catch (err) {
+                this.$notify({ type: "error", text: "Error connecting to the serve." });
+            } finally {
+                this.loading = false;
+            }
+        },
+        async decodeUrl() {
+            this.loading = true;
+            try {
+                let response = await axios.post("/url_shortener/decode/", {short_url: this.urlToDecode.replace(settings.API_URL, '')});
+                if(!response.data.success) {
+                    this.$notify({ type: "error", text: response.data.message });
+                } else {
+                    this.urlToDecode = null;
+                    this.decodedUrl = response.data.url.long_url
                 }
             } catch (err) {
                 this.$notify({ type: "error", text: "Error connecting to the serve." });
